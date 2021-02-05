559156bff8bc2ec26af34bf7a1c24d442a0f9ccb
f0lool <hadulinpositive@gmail.com>
2021-02-05 11:17:19 +0700

    lesson-3

---
+++ lesson-3/Домашнее Задание №3/Задание1/Program.cs
@@ -0,0 +1,219 @@
+﻿#region Автор
+// Червов Алексей
+#endregion
+
+#region Описание задания
+/*
+1. а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры;
+б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;
+в) Добавить диалог с использованием switch демонстрирующий работу класса.
+*/
+#endregion
+
+#region Библиотеки
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+namespace Задание1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Чтобы вы хотели посмотреть?\n" +
+                "1. Структуру комлексных чисел\n" +
+                "2. Класс комплексных чисел\n");
+
+            var switchComplex = int.Parse(Console.ReadLine());
+
+            switch (switchComplex)
+            {
+                case 1:
+                    ComplexS complexOne;
+                    Console.Write("Введите вещественную часть первого числа: ");
+                    complexOne.re = int.Parse(Console.ReadLine());
+                    Console.Write("Введите мнимую часть первого числа: ");
+                    complexOne.im = int.Parse(Console.ReadLine());
+
+                    ComplexS complexTwo;
+                    Console.Write("Введите вещественную часть второго числа: ");
+                    complexTwo.re = int.Parse(Console.ReadLine());
+                    Console.Write("Введите мнимую часть второго числа: ");
+                    complexTwo.im = int.Parse(Console.ReadLine());
+
+                    Console.Write("Что вы хотите сделать с этими числами?\n" +
+                        "1. Сложить\n" +
+                        "2. Вычесть\n" +
+                        "3. Умножить\n");
+
+                    var switchOperator = int.Parse(Console.ReadLine());
+                    ComplexS result;
+
+                    switch (switchOperator)
+                    {
+                        case 1:
+                            result = complexOne.Plus(complexTwo);
+                            break;
+                        case 2:
+                            result = complexOne.Minus(complexTwo);
+                            break;
+                        case 3:
+                            result = complexOne.Multi(complexTwo);
+                            break;
+                        default:
+                            result.re = 0;
+                            result.im = 0;
+                            break;
+                    }
+
+                    Console.Write($"{result.re} + {result.im}i");
+                    break;
+                case 2:
+                    Complex complexFirst = new Complex();
+                    Console.Write("Введите вещественную часть первого числа: ");
+                    complexFirst.Re = double.Parse(Console.ReadLine());
+                    Console.Write("Введите мнимую часть первого числа: ");
+                    complexFirst.Im = double.Parse(Console.ReadLine());
+
+                    Complex complexSecond = new Complex();
+                    Console.Write("Введите вещественную часть второго числа: ");
+                    complexSecond.Re = double.Parse(Console.ReadLine());
+                    Console.Write("Введите мнимую часть второго числа: ");
+                    complexSecond.Im = double.Parse(Console.ReadLine());
+
+                    Console.Write("Что вы хотите сделать с этими числами?\n" +
+                        "1. Сложить\n" +
+                        "2. Вычесть\n" +
+                        "3. Умножить\n");
+
+                    switchOperator = int.Parse(Console.ReadLine());
+                    Complex resultClass = new Complex();
+
+                    switch (switchOperator)
+                    {
+                        case 1:
+                            resultClass = complexFirst.Plus(complexSecond);
+                            break;
+                        case 2:
+                            resultClass = complexFirst.Minus(complexSecond);
+                            break;
+                        case 3:
+                            resultClass = complexFirst.Multi(complexSecond);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    Console.Write($"{resultClass.Re} + {resultClass.Im}i");
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Complex
+    {
+        private double im;
+        double re;
+
+        public Complex()
+        {
+            im = 0;
+            re = 0;
+        }
+
+        public Complex(double _im, double re)
+        {
+            im = _im;
+            this.re = re;
+        }
+        public Complex Plus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = x2.im + im;
+            x3.re = x2.re + re;
+            return x3;
+        }
+
+        public Complex Multi(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = re * x2.im + im * x2.re;
+            x3.re = re * x2.re - im * x2.im;
+
+            return x3;
+        }
+
+        public Complex Minus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = im - x2.im;
+            x3.re = re - x2.re;
+
+            return x3;
+        }
+        public double Im
+        {
+            get { return im; }
+            set
+            {
+                if (value >= 0) im = value;
+            }
+        }
+
+        public double Re
+        {
+            get { return re; }
+            set
+            {
+                if (value >= 0) re = value;
+            }
+        }
+        public string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+
+    struct ComplexS
+    {
+        public double im;
+        public double re;
+        public ComplexS Plus(ComplexS x)
+        {
+            ComplexS y;
+            y.im = im + x.im;
+            y.re = re + x.re;
+            return y;
+        }
+        public ComplexS Multi(ComplexS x)
+        {
+            ComplexS y;
+            y.im = re * x.im + im * x.re;
+            y.re = re * x.re - im * x.im;
+            return y;
+        }
+
+        public ComplexS Minus(ComplexS x2)
+        {
+            ComplexS x3;
+            x3.im = im - x2.im;
+            x3.re = re - x2.re;
+
+            return x3;
+        }
+        public string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+
+}
+
+
---
+++ lesson-3/Домашнее Задание №3/Задание3/Program.cs
@@ -0,0 +1,186 @@
+﻿#region Автор
+// Червов Алексей
+#endregion
+
+#region Описание задания
+/*
+3. *Описать класс дробей - рациональных чисел, являющихся отношением двух целых чисел. 
+Предусмотреть методы сложения, вычитания, умножения и деления дробей. 
+Написать программу, демонстрирующую все разработанные элементы класса.
+** Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение
+ArgumentException("Знаменатель не может быть равен 0");
+Добавить упрощение дробей.
+*/
+#endregion
+
+#region Библиотеки
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+namespace Задание3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fractions fractionOne = new Fractions();
+            Console.Write("Введите числитель первой дроби: ");
+            fractionOne.Numerator = int.Parse(Console.ReadLine());
+            Console.Write("Введите знаменатель первой дроби: ");
+            fractionOne.Denominator = int.Parse(Console.ReadLine());
+
+            Fractions fractionTwo = new Fractions();
+            Console.Write("Введите числитель второй дроби: ");
+            fractionTwo.Numerator = int.Parse(Console.ReadLine());
+            Console.Write("Введите знаменатель второй дроби: ");
+            fractionTwo.Denominator = int.Parse(Console.ReadLine());
+
+
+
+            Console.Write("Что вы хотите сделать с дробями:\n" +
+                "1.Сложить\n" +
+                "2.Вычесть\n" +
+                "3.Умножить\n" +
+                "4.Разделить\n");
+
+            var switchNumber = int.Parse(Console.ReadLine());
+            var result = fractionOne.Plus(fractionTwo);
+
+            switch (switchNumber)
+            {
+                case 1:
+                    result = fractionOne.Plus(fractionTwo);
+                    break;
+                case 2:
+                    result = fractionOne.Minus(fractionTwo);
+                    break;
+                case 3:
+                    result = fractionOne.Multi(fractionTwo);
+                    break;
+                case 4:
+                    result = fractionOne.Division(fractionTwo);
+                    break;
+                default:
+                    Console.Write("Вы ввели некорректное значение. Ваши дроби, по умолчанию, будут сложенны");
+                    break;
+            }
+
+            for (int i = 2; i <= Math.Min(result.Numerator, result.Denominator); i++)
+            {
+                if (result.Numerator % i == 0 && result.Denominator % i == 0)
+                {
+                    result.Numerator = result.Numerator / i;
+                    result.Denominator = result.Denominator / i;
+                }
+            }
+
+            Console.WriteLine($"{result.Numerator}/{result.Denominator} or {result.DecimalFraction:f2}");
+
+            Console.ReadKey();
+        }
+    }
+    class Fractions
+    {
+        private int numerator;
+        public int Numerator
+        {
+            set { numerator = value; }
+            get { return numerator; }
+        }
+
+        private int denominator;
+
+        public int Denominator
+        {
+            set
+            {
+                try
+                {
+                    denominator = value;
+                    if (denominator == 0)
+                        throw new ArgumentException();
+                }
+                catch (ArgumentException)
+                {
+                    while (denominator == 0)
+                    {
+                        Console.Write("Знаменатель не может быть равен нулю. Введите значение отличное от нуля: ");
+                        denominator = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+            get { return denominator; }
+        }
+
+        private double decimalFraction;
+
+        public double DecimalFraction
+        {
+            get { return decimalFraction; }
+        }
+        public Fractions Plus(Fractions fractionOne)
+        {
+            Fractions fractionTwo = new Fractions();
+            var nok = (fractionOne.denominator * denominator) / Gcd(fractionOne.denominator, denominator);
+
+            fractionTwo.numerator = numerator * (nok / denominator) + fractionOne.numerator * (nok / fractionOne.denominator);
+            fractionTwo.denominator = nok;
+            fractionTwo.decimalFraction = (double)fractionTwo.numerator / fractionTwo.denominator;
+
+            return fractionTwo;
+        }
+
+        public Fractions Minus(Fractions fractionOne)
+        {
+            Fractions fractionTwo = new Fractions();
+            var nok = (fractionOne.denominator * denominator) / Gcd(fractionOne.denominator, denominator);
+
+            fractionTwo.numerator = numerator * (nok / denominator) - fractionOne.numerator * (nok / fractionOne.denominator);
+            fractionTwo.denominator = nok;
+            fractionTwo.decimalFraction = (double)fractionTwo.numerator / fractionTwo.denominator;
+
+            return fractionTwo;
+        }
+
+        public Fractions Multi(Fractions fractionOne)
+        {
+            Fractions fractionTwo = new Fractions();
+
+            fractionTwo.numerator = numerator * fractionOne.numerator;
+            fractionTwo.denominator = denominator * fractionOne.denominator;
+            fractionTwo.decimalFraction = (double)fractionTwo.numerator / fractionTwo.denominator;
+
+            return fractionTwo;
+        }
+
+        public Fractions Division(Fractions fractionOne)
+        {
+            Fractions fractionTwo = new Fractions();
+
+            fractionTwo.numerator = numerator * fractionOne.denominator;
+            fractionTwo.denominator = denominator * fractionOne.numerator;
+            fractionTwo.decimalFraction = (double)fractionTwo.numerator / fractionTwo.denominator;
+
+            return fractionTwo;
+        }
+
+        public int Gcd(int _a, int _b)
+        {
+            var a = _a;
+            var b = _b;
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a = a % b;
+                else
+                    b = b % a;
+            }
+
+            return a + b;
+        }
+    }
+}
