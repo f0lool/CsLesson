01b49ba4e7daf31c4cedd2c2641100727ac900f1
f0lool <hadulinpositive@gmail.com>
2021-02-16 07:58:17 +0700

    lesson-5

---
+++ lesson-5/Домашнее Задание №5/Задание3/Program.cs
@@ -0,0 +1,53 @@
+﻿#region Автор
+// Червов Алексей
+#endregion
+
+#region Описание задания
+/*
+3. *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой. Регистр можно не учитывать:
+б) *разработав собственный алгоритм.
+Например:
+badc являются перестановкой abcd.
+*/
+#endregion
+
+#region Библиотеки
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+#endregion
+
+namespace Задание3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую строку: ");
+            string text1 = Console.ReadLine();
+            Console.Write("Введите вторую строку: ");
+            string text2 = Console.ReadLine();
+            int count = 0;
+
+            if (text1.Length == text2.Length)
+                for (int i = 0; i < text1.Length; i++)
+                    for (int j = 0; j < text2.Length; j++)
+                        if (text1[i] == text2[j])
+                        {
+                            count++;
+                            break;
+                        }
+
+            if (count == text1.Length)
+                Console.WriteLine("Эти две строки являются перестановкой друг друга");
+            else
+                Console.WriteLine("Эти две строки не являются перестановкой друг друга");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lesson-5/Домашнее Задание №5/Задание5/Program.cs
@@ -0,0 +1,85 @@
+﻿#region Автор
+// Червов Алексей
+#endregion
+
+#region Описание задания
+/*
+5. **Написать игру «Верю. Не верю». В файле хранятся вопрос и ответ, правда это или нет. 
+Например: «Шариковую ручку изобрели в древнем Египте», «Да». Компьютер загружает эти данные, случайным образом выбирает 5 вопросов и задаёт их игроку. 
+Игрок отвечает Да или Нет на каждый вопрос и набирает баллы за каждый правильный ответ. Список вопросов ищите во вложении или воспользуйтесь интернетом.
+*/
+#endregion
+
+#region Библиотеки
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+#endregion
+
+namespace Задание5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathQuestions = @"C:\Users\user\source\repos\Домашнее Задание №5\Задание5\questions.txt";
+            string pathAnswer = @"C:\Users\user\source\repos\Домашнее Задание №5\Задание5\answer.txt";
+            int score = 0;
+            string answer;
+            Random random = new Random();
+            int count;
+
+            List<GameInfo> gameInfo = new List<GameInfo>();
+
+            GameInfo.Read(pathQuestions, pathAnswer, gameInfo);
+
+            for (int i = 0; i < 5; i++)
+            {
+                count = random.Next(1, gameInfo.Count());
+                Console.WriteLine(gameInfo[count].question);
+                answer = Console.ReadLine();
+                if (answer.ToLower() == gameInfo[count].answer.ToLower())
+                {
+                    score++;
+                    Console.WriteLine("Верно");
+                    gameInfo.RemoveAt(count);
+                }
+                else
+                {
+                    Console.WriteLine("Не верно");
+                    gameInfo.RemoveAt(count);
+                }
+                Console.WriteLine($"Ваше количество баллов: {score}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+    class GameInfo
+    {
+        public string question;
+        public string answer;
+
+        public static void Read(string pathQuestion, string pathAnswer, List<GameInfo> gameInfo)
+        {
+            StreamReader streamReaderQ = new StreamReader(pathQuestion);
+            StreamReader streamReaderA = new StreamReader(pathAnswer);
+
+            while (!streamReaderQ.EndOfStream && !streamReaderA.EndOfStream)
+            {
+                GameInfo info = new GameInfo();
+                info.question = streamReaderQ.ReadLine();
+                info.answer = streamReaderA.ReadLine();
+
+                gameInfo.Add(info);
+            }
+
+            streamReaderA.Close();
+            streamReaderQ.Close();
+        }
+    }
+}
